ff76b0be8141be27c2c8298436a6f0e027f43dbe
Javier <jmartin@grupoloyal.com>
2024-03-13 10:46:06 +0100

    linter

--- src/ejemplos.js
+++ src/ejemplos.js
@@ -9,9 +9,12 @@ message = `Hola ${nombre} sigo salto a otra ${a + +b} ${a * b}`
 // Update header text
 document.querySelector('#header').innerHTML = message
 message = true
+// eslint-disable-next-line no-cond-assign
+
 if(message) {
   console.log('Es boolean')
 } else
     console.log('No es boolean')
 // Log to console
+message = a = b = 'c4'
 console.log(message)
