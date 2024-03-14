1397d1a57576aee9479fb335f7cef2c873ed5b9b
Javier <jmartin@grupoloyal.com>
2024-03-14 08:45:49 +0100

    soluciones

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -2,6 +2,7 @@ const aleatorio = (inicio, rango) => {
     return parseInt((inicio + Math.random() * rango))
 }
 
+// eslint-disable-next-line no-unused-vars
 const creaArray = (elementos, value = 0) => {
     let t = []
     for (let i = 0; i < elementos; i++)
@@ -9,6 +10,54 @@ const creaArray = (elementos, value = 0) => {
     return t
 }
 
+// eslint-disable-next-line no-unused-vars
+function damePrimos(num) {
+    var calculos = 0;
+    var cuantos = +num;
+    var rslt = [];
+    var candidato = 2;
+    while (cuantos) {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+            var esPrimo = true;
+            for (var indice in rslt) {
+                calculos++;
+                if (candidato % rslt[indice] == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            candidato++;
+            if (esPrimo) {
+                rslt.push(candidato - 1);
+                break;
+            }
+        }
+        cuantos--;
+    }
+    console.log('Coste función: ' + calculos);
+    return rslt;
+}
+
+function esNIF(nif) {
+    if (!/^\d{1,8}[A-Za-z]$/.test(nif))
+        return false;
+    const letterValue = nif.substr(nif.length - 1);
+    const numberValue = nif.substr(0, nif.length - 1);
+    return letterValue.toUpperCase() === 'TRWAGMYFPDXBNJZSQVHLCKE'.charAt(numberValue % 23);
+}
+
+function esPalindromo(cadena) {
+    if (typeof (cadena) != "string" || cadena.trim().length == 0) return false;
+    cadena = cadena.replace(/[ .,;:#¿?¡!()[\]{}=+\-*/_`~$%^&'"]/g, '').normalize("NFD").replace(/[\u0300-\u036f]/g, '').toLowerCase();
+    for (let i = 0; i < cadena.length - i; i++) {
+        if (cadena.charAt(i) !== cadena.charAt(cadena.length - 1 - i)) return false;
+    }
+    return true;
+}
+
+// eslint-disable-next-line no-unused-vars
 class Juego {
     constructor() {
         this.intentos = 10
